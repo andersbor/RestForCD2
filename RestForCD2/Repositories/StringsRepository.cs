6be19da9fe9afd24f3e0145e6bbce89fa7bff1fc
Anders Børjesson <anbo@zealand.dk>
2023-03-15 12:12:05 +0100

    strings controller: get all + post

---
+++ RestForCD2/Repositories/StringsRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace RestForCD2.Repositories
+{
+    public class StringsRepository
+    {
+        private readonly List<string> _strings = new() { "Hello", "World" };
+
+        public void Add(string value)
+        {
+            _strings.Add(value);
+        }
+
+        public IEnumerable<string> GetAll()
+        {
+            return new List<string>(_strings);
+        }
+    }
+}
